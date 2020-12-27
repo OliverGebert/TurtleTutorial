4d7bf17a7a6c52663b75d14cae7dbd88dd22b700
Oli G <oli@Macintosh.fritz.box>
2020-12-27 16:45:50 +0100

    if clause

--- TurtleTutorial/TurtleProject/src/Turtle1.py
+++ TurtleTutorial/TurtleProject/src/Turtle1.py
@@ -5,11 +5,20 @@ welt = TurtleWorld()
 tim = Turtle()
 print(tim)
 
+def quadrat(turtle, breite):
+    """ zeichnet ein Quadrat mit gegebener Breite """
+    if breite>0:
+        for i in range(4):
+            fd(turtle,int(breite))
+            quadrat(turtle, breite -2)
+            if breite%2 == 0:
+                lt(turtle)
+            else:
+                rt(turtle)
+
 fd(tim, 100)
-lt(tim)
+QuadratBreite = input('wie breit soll das Quadrat werden? ')
+quadrat(tim, int(QuadratBreite))
+fd(tim,100)
 
-QuadratBreite = input('wie breit solldas Quadrat werden?')
-
-fd(tim,int(QuadratBreite))
-
-time.sleep(5)
+time.sleep(3)
