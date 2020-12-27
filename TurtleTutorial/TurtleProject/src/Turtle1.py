c227a3f161f4438588d0c133fce889c75c6c6b5e
Oli G <oli@Macintosh.fritz.box>
2020-12-27 17:22:31 +0100

    recursion

--- TurtleTutorial/TurtleProject/src/Turtle1.py
+++ TurtleTutorial/TurtleProject/src/Turtle1.py
@@ -5,12 +5,23 @@ welt = TurtleWorld()
 tim = Turtle()
 print(tim)
 
+def fakultaet(n):
+    space = ' ' * n
+    print(space, 'Fakultät: ', n)
+    if n < 0:
+        print("Fakultät kann nur von positiven ganzen Zahlen berechnet werden")
+        return None
+    elif n == 0:
+        return 1
+    else:
+        return n*fakultaet(n-1)
+
 def quadrat(turtle, breite):
     """ zeichnet ein Quadrat mit gegebener Breite """
-    if breite>0:
+    if breite > 0:
         for i in range(4):
             fd(turtle,int(breite))
-            quadrat(turtle, breite -2)
+            quadrat(turtle, breite -5)
             if breite%2 == 0:
                 lt(turtle)
             else:
@@ -18,7 +29,8 @@ def quadrat(turtle, breite):
 
 fd(tim, 100)
 QuadratBreite = input('wie breit soll das Quadrat werden? ')
+print(fakultaet(int(QuadratBreite)))
 quadrat(tim, int(QuadratBreite))
 fd(tim,100)
 
-time.sleep(3)
+time.sleep(5)
